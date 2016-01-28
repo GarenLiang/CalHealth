e6d1dc935da028cd543b95cd0a0e5f0baeb8cc89
JackLiang1 <ideabreeze@gmail.com>
2016-01-27 21:59:29 -0600

    Update ExploringData.R

--- ExploringData.R
+++ ExploringData.R
@@ -1,3 +1,13 @@
+#RBMI: BMI Category description
+#BMI_P: BMI value
+#RACEHPR2: race
+#SRSEX: sex
+#SRAGE_P: age
+#MARIT2: Marital status
+#AB1: General Health Condition
+#ASTCUR: Current Asthma Status
+#AB51: Type I or Type II Diabetes
+#POVLL: Poverty level
 # CalHealth
 # Explore the dataset with summary and str
 summary(adult)
