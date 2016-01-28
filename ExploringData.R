3a0e5b3b43f07022561d18865a38b91b04737935
JackLiang1 <ideabreeze@gmail.com>
2016-01-27 21:57:40 -0600

    Update ExploringData.R

--- ExploringData.R
+++ ExploringData.R
@@ -15,6 +15,4 @@ ggplot(adult, aes (x = BMI_P)) +
 ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
   geom_histogram(binwidth = 1)
 
-![5kl3dr4hpk1zlganaiafdyd5 8n57b4feo6hciaunabs0abiqqmgbq2afdqautd h9ojf sjjeu6aaaaaelftksuqmcc](https://cloud.githubusercontent.com/assets/15516507/12634605/ad69ad08-c540-11e5-951a-ab0506725ab5.png)
-![anpgd dgkuacqgqqggwrpdvanpdex2frybqrybqrybqrybqrybqrybq9x mhiekxtkhyaaaaabjru5erkjggg](https://cloud.githubusercontent.com/assets/15516507/12634610/b78367b6-c540-11e5-88c0-5d5102740e56.png)
-![b8orahqeqgo0an6l3n2u15tn7xj0aeinbkbqnabsowgqgg2a1ggapeb7fx9ou74vre6laaaaaelftksuqmcc](https://cloud.githubusercontent.com/assets/15516507/12634611/bb9d4074-c540-11e5-842c-0b8a88056465.png)
+
